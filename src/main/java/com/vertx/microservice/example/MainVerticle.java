61bebe9a4fd83ba40e0bd4bebd0438bb0f5ec0e0
David Parro <davidparro@gmail.com>
2021-03-26 13:28:07 +0100

    Config updated

--- src/main/java/com/vertx/microservice/example/MainVerticle.java
+++ src/main/java/com/vertx/microservice/example/MainVerticle.java
@@ -5,6 +5,7 @@ import io.vertx.config.ConfigRetriever;
 import io.vertx.config.ConfigRetrieverOptions;
 import io.vertx.config.ConfigStoreOptions;
 import io.vertx.core.AbstractVerticle;
+import io.vertx.core.AsyncResult;
 import io.vertx.core.Promise;
 import io.vertx.core.Vertx;
 import io.vertx.core.http.HttpServer;
@@ -20,14 +21,18 @@ public class MainVerticle extends AbstractVerticle {
 
         ConfigRetriever retriever = ConfigRetriever.create(vertx);
 
-        retriever.getConfig(json -> {
-            this.createMongoClient();
-            HttpServer server = vertx.createHttpServer();
-            Router mainRouter = Router.router(vertx);
+        retriever.getConfig(config -> {
+            if (config.failed()) {
+                startPromise.fail(config.cause());
+            } else {
+                this.createMongoClient(config);
+                HttpServer server = vertx.createHttpServer();
+                Router mainRouter = Router.router(vertx);
 
-            mainRouter.route("/ruta/:productID").handler(ProductHandler::handleGetProduct);
+                mainRouter.route("/ruta/:productID").handler(ProductHandler::handleGetProduct);
 
-            server.requestHandler(mainRouter).listen(8080);
+                server.requestHandler(mainRouter).listen(config.result().getInteger("port"));
+            }
         });
 
         /*
@@ -64,23 +69,18 @@ public class MainVerticle extends AbstractVerticle {
         });*/
     }
 
-    private void createMongoClient() {
-        JsonObject config = Vertx.currentContext().config();
+    private void createMongoClient(AsyncResult<JsonObject> config) {
 
-        String uri = config.getString("mongo_uri");
-        System.out.println("MONGO URI: " + uri);
-        if (uri == null) {
-            uri = "mongodb://localhost:27017";
-        }
-        String db = config.getString("mongo_db");
-        System.out.println("MONGO DB: " + db);
-        if (db == null) {
-            db = "test";
-        }
+        String uri = config.result().getString("mongo.mongo_uri");
+        String db = config.result().getString("mongo.mongo_db");
+        // String user = config.result().getString("mongo.username");
+        // String pass = config.result().getString("mongo.password");
 
         JsonObject mongoconfig = new JsonObject()
                 .put("connection_string", uri)
                 .put("db_name", db);
+                /*.put("username", user)
+                .put("password", pass);*/
 
         MongoClient mongo = MongoClient.createShared(vertx, mongoconfig);
 
@@ -92,17 +92,18 @@ public class MainVerticle extends AbstractVerticle {
 
         mongoClient.save("products", product1, id -> {
             System.out.println("Inserted id: " + id.result());
+            System.out.println(id);
 
-            /*mongoClient.find("products", new JsonObject().put("id", "12345"), res -> {
+            mongoClient.find("products", new JsonObject().put("id", "12345"), res -> {
                 System.out.println("Name is " + res.result().get(0).getString("name"));
 
-                mongoClient.remove("products", new JsonObject().put("id", "12345"), rs -> {
+                mongoClient.removeDocuments("products", new JsonObject().put("id", "12345"), rs -> {
                     if (rs.succeeded()) {
                         System.out.println("Product removed ");
                     }
                 });
 
-            });*/
+            });
 
         });
     }
